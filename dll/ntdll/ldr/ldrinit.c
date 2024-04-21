4decc15f04fb070a13d3f78513c8a652bbda5569
Justin Miller <justin.miller@reactos.org>
2024-04-21 16:23:27 -0700

    [NTDLL][KERNEL32][SUBSYSTEMS][NTOS][SDK] Fix usages of the follow NT5 flags: HasFiberData InDbgPrint FreeStackOnTermination

--- dll/ntdll/ldr/ldrinit.c
+++ dll/ntdll/ldr/ldrinit.c
@@ -1250,7 +1250,11 @@ LdrShutdownThread(VOID)
     }
 
     /* Check for Fiber data */
-    if (Teb->HasFiberData)
+#if (NTDDI_VERSION < NTDDI_LONGHORN)
+    if (!Teb->HasFiberData)
+#else
+    if (!Teb->DbgHasFiberData)
+#endif
     {
         /* Free Fiber data*/
         RtlFreeHeap(RtlGetProcessHeap(), 0, Teb->NtTib.FiberData);
