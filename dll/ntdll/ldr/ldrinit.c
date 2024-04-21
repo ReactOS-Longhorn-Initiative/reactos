447dcd9e899c2eb1901690168b6e4b789cc9ec30
Justin Miller <justin.miller@reactos.org>
2024-04-21 16:27:56 -0700

    [NTDLL] Removed usage of TraceLogging, not that we support etw anyway

--- dll/ntdll/ldr/ldrinit.c
+++ dll/ntdll/ldr/ldrinit.c
@@ -983,12 +983,14 @@ LdrShutdownProcess(VOID)
     /* Enter the Loader Lock */
     RtlEnterCriticalSection(&LdrpLoaderLock);
 
+#if (NTDDI_VERSION < NTDDI_VISTA)
     /* Cleanup trace logging data (Etw) */
     if (SharedUserData->TraceLogging)
     {
         /* FIXME */
         DPRINT1("We don't support Etw yet.\n");
     }
+#endif
 
     /* Start at the end */
     ListHead = &Peb->Ldr->InInitializationOrderModuleList;
@@ -1102,12 +1104,14 @@ LdrShutdownThread(VOID)
     DPRINT("LdrShutdownThread() called for %wZ\n",
             &LdrpImageEntry->BaseDllName);
 
+#if (NTDDI_VERSION < NTDDI_VISTA)
     /* Cleanup trace logging data (Etw) */
     if (SharedUserData->TraceLogging)
     {
         /* FIXME */
         DPRINT1("We don't support Etw yet.\n");
     }
+#endif
 
     /* Get the Ldr Lock */
     RtlEnterCriticalSection(&LdrpLoaderLock);
