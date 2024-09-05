976f55f033749266cd8f855659feb6e59e6e42d7
Justin Miller <justin.miller@reactos.org>
2024-09-04 17:03:53 -0700

    Import Shellitem from WINE Begin CShellLibrary Impl from Oleg Begin CUserEventTimer Impl From Oleg

--- dll/win32/shell32/wine/pidl.c
+++ dll/win32/shell32/wine/pidl.c
@@ -1517,7 +1517,7 @@ HRESULT WINAPI SHGetNameFromIDList(PCIDLIST_ABSOLUTE pidl, SIGDN sigdnName, PWST
     return ret;
 }
 
-#ifndef __REACTOS__
+//#ifndef __REACTOS__
 
 /*************************************************************************
  * SHGetIDListFromObject             [SHELL32.@]
@@ -1595,7 +1595,7 @@ HRESULT WINAPI SHGetIDListFromObject(IUnknown *punk, PIDLIST_ABSOLUTE *ppidl)
     return ret;
 }
 
-#endif /* !__REACTOS__ */
+//#endif /* !__REACTOS__ */
 
 /**************************************************************************
  *
