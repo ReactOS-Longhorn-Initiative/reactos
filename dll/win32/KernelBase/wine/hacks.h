7f45b816b9e20d14acfaeb0a5d95f4b53716fd97
Justin Miller <Justin.miller@reactos.org>
2024-09-08 01:14:39 -0700

    [KERNELBASE] Volume.c now builds

--- dll/win32/KernelBase/wine/hacks.h
+++ dll/win32/KernelBase/wine/hacks.h
@@ -6,7 +6,28 @@
 #define RESOURCE_ENUM_MUI_SYSTEM  0x0004
 #define RESOURCE_ENUM_VALIDATE    0x0008
 
+//ddk
+#define SYMBOLIC_LINK_ALL_ACCESS        (STANDARD_RIGHTS_REQUIRED | 0x1)
 
+//def ndk
 NTSYSAPI NTSTATUS  WINAPI RtlQueryActivationContextApplicationSettings(DWORD,HANDLE,const WCHAR*,const WCHAR*,WCHAR*,SIZE_T,SIZE_T*);
 NTSYSAPI NTSTATUS  WINAPI LdrSetDefaultDllDirectories(ULONG);
 NTSYSAPI NTSTATUS  WINAPI LdrRemoveDllDirectory(void*);
+
+
+//rtl types
+//
+// Constant String Macro
+//
+#define RTL_CONSTANT_STRING(__SOURCE_STRING__)                  \
+{                                                               \
+    sizeof(__SOURCE_STRING__) - sizeof((__SOURCE_STRING__)[0]), \
+    sizeof(__SOURCE_STRING__),                                  \
+    (__SOURCE_STRING__)                                         \
+}
+
+//kernel32
+
+BOOL WINAPI GetVolumeInformationByHandleW( HANDLE handle, WCHAR *label, DWORD label_len,
+                                           DWORD *serial, DWORD *filename_len, DWORD *flags,
+                                           WCHAR *fsname, DWORD fsname_len );
