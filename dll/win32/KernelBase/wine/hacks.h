d904e4fcd935f1776fa90e31c04d94cf235312d8
Justin Miller <Justin.miller@reactos.org>
2024-09-08 01:18:52 -0700

    [KERNELBASE] Version.c now builds

--- dll/win32/KernelBase/wine/hacks.h
+++ dll/win32/KernelBase/wine/hacks.h
@@ -5,11 +5,21 @@
 #define RESOURCE_ENUM_MUI         0x0002
 #define RESOURCE_ENUM_MUI_SYSTEM  0x0004
 #define RESOURCE_ENUM_VALIDATE    0x0008
-
+#define PROCESSOR_ARCHITECTURE_NEUTRAL          11
+#define PROCESSOR_ARCHITECTURE_ARM64            12
 //ddk
 #define SYMBOLIC_LINK_ALL_ACCESS        (STANDARD_RIGHTS_REQUIRED | 0x1)
 
 //def ndk
+NTSYSAPI
+BOOLEAN
+NTAPI
+RtlGetProductInfo(
+  _In_ ULONG OSMajorVersion,
+  _In_ ULONG OSMinorVersion,
+  _In_ ULONG SpMajorVersion,
+  _In_ ULONG SpMinorVersion,
+  _Out_ PULONG ReturnedProductType);
 NTSYSAPI NTSTATUS  WINAPI RtlQueryActivationContextApplicationSettings(DWORD,HANDLE,const WCHAR*,const WCHAR*,WCHAR*,SIZE_T,SIZE_T*);
 NTSYSAPI NTSTATUS  WINAPI LdrSetDefaultDllDirectories(ULONG);
 NTSYSAPI NTSTATUS  WINAPI LdrRemoveDllDirectory(void*);
@@ -27,6 +37,8 @@ NTSYSAPI NTSTATUS  WINAPI LdrRemoveDllDirectory(void*);
 }
 
 //kernel32
+
+
 
 BOOL WINAPI GetVolumeInformationByHandleW( HANDLE handle, WCHAR *label, DWORD label_len,
                                            DWORD *serial, DWORD *filename_len, DWORD *flags,
