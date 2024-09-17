986bb5ee9b77732e1b5857af4e4a74fa0ddf6246
winesync <ros-dev@reactos.org>
2024-09-17 04:26:57 -0700

    [WINESYNC] comctl32/treeview: Don't repaint when handling WM_STYLECHANGED.
    Some applications subclass treeview and change style multiple times when painting. Invalidating
    control when handling WM_STYLECHANGED causes infinite WM_PAINT messages in this case and end up
    stalling the message queue.
    
    wine commit id 44cfa3ec1af33f0f2560878c269405468f999a70 by Zhiyi Zhang <zzhang@codeweavers.com>

--- dll/win32/comctl32/treeview.c
+++ dll/win32/comctl32/treeview.c
@@ -5658,7 +5658,6 @@ TREEVIEW_StyleChanged(TREEVIEW_INFO *infoPtr, WPARAM wParam, LPARAM lParam)
     TREEVIEW_EndEditLabelNow(infoPtr, TRUE);
     TREEVIEW_UpdateSubTree(infoPtr, infoPtr->root);
     TREEVIEW_UpdateScrollBars(infoPtr);
-    TREEVIEW_Invalidate(infoPtr, NULL);
 
     return 0;
 }
