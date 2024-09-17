e969ac92d5d489eac3d835b7e9773bc714c579f4
winesync <ros-dev@reactos.org>
2024-09-17 03:43:31 -0700

    [WINESYNC] comctl32: Check control type in the STM_SETIMAGE/STM_SETICON handlers before calling the helpers.
    Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 9b7faf98a9853cece4ecc2021943b97706f0dd01 by Dmitry Timoshkov <dmitry@baikal.ru>

--- dll/win32/comctl32/static.c
+++ dll/win32/comctl32/static.c
@@ -146,7 +146,6 @@ static HICON STATIC_SetIcon( HWND hwnd, HICON hicon, DWORD style )
     SIZE size;
     struct static_extra_info *extra;
 
-    if ((style & SS_TYPEMASK) != SS_ICON) return 0;
     if (hicon && !get_icon_size( hicon, &size ))
     {
         WARN("hicon != 0, but invalid\n");
@@ -233,7 +232,6 @@ static HBITMAP STATIC_SetBitmap( HWND hwnd, HBITMAP hBitmap, DWORD style )
     HBITMAP hOldBitmap, alpha;
     struct static_extra_info *extra;
 
-    if ((style & SS_TYPEMASK) != SS_BITMAP) return 0;
     if (hBitmap && GetObjectType(hBitmap) != OBJ_BITMAP)
     {
         WARN("hBitmap != 0, but it's not a bitmap\n");
@@ -290,7 +288,6 @@ static HENHMETAFILE STATIC_SetEnhMetaFile( HWND hwnd, HENHMETAFILE hEnhMetaFile,
     HENHMETAFILE old_hemf;
     struct static_extra_info *extra;
 
-    if ((style & SS_TYPEMASK) != SS_ENHMETAFILE) return 0;
     if (hEnhMetaFile && GetObjectType(hEnhMetaFile) != OBJ_ENHMETAFILE)
     {
         WARN("hEnhMetaFile != 0, but it's not an enhanced metafile\n");
@@ -607,13 +604,16 @@ static LRESULT CALLBACK STATIC_WindowProc( HWND hwnd, UINT uMsg, WPARAM wParam, 
         switch (wParam)
         {
         case IMAGE_BITMAP:
+            if (style != SS_BITMAP) return 0;
             lResult = (LRESULT)STATIC_SetBitmap( hwnd, (HBITMAP)lParam, full_style );
             break;
         case IMAGE_ENHMETAFILE:
+            if (style != SS_ENHMETAFILE) return 0;
             lResult = (LRESULT)STATIC_SetEnhMetaFile( hwnd, (HENHMETAFILE)lParam, full_style );
             break;
         case IMAGE_ICON:
         case IMAGE_CURSOR:
+            if (style != SS_ICON) return 0;
             lResult = (LRESULT)STATIC_SetIcon( hwnd, (HICON)lParam, full_style );
             break;
         default:
@@ -624,6 +624,7 @@ static LRESULT CALLBACK STATIC_WindowProc( HWND hwnd, UINT uMsg, WPARAM wParam, 
         break;
 
     case STM_SETICON:
+        if (style != SS_ICON) return 0;
         lResult = (LRESULT)STATIC_SetIcon( hwnd, (HICON)wParam, full_style );
         STATIC_TryPaintFcn( hwnd, full_style );
         break;
