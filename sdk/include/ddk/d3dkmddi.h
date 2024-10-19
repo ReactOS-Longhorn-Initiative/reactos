da8c1211b9a6d03df9e52dfc55eec04fa8b6f460
Justin Miller <justin.miller@reactos.org>
2024-10-19 14:33:00 -0700

    [SDK] Create Empty headers for remainder of WDDM architecture - Create Displib

---
+++ sdk/include/ddk/d3dkmddi.h
@@ -0,0 +1,13 @@
+/*
+ * PROJECT:     ReactOS Display Driver Model
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Header file for WDDM style DDIs
+ * COPYRIGHT:   Copyright 2024 Justin Miller <justin.miller@reactos.org>
+ */
+
+#ifndef _D3DKMDDI_H_
+#define _D3DKMDDI_H_
+
+#include <d3dkmdt.h>
+
+#endif // _D3DKMDDI_H_
