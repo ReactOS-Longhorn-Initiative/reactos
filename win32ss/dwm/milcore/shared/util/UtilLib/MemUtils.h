f04e39aae346cf0c5687c07c9d35c371a16bb46f
Mikhail <mishakeys20@gmail.com>
2025-05-08 16:18:50 -0400

    [MILCORE] fix utillib msvc, import some headers

--- win32ss/dwm/milcore/shared/util/UtilLib/MemUtils.h
+++ win32ss/dwm/milcore/shared/util/UtilLib/MemUtils.h
@@ -156,10 +156,10 @@ inline void Free(__inout_ecount(1) Heap* pheap, void * pv);
 //      MtSetDefault(Mt(foo));
 //      SomeRandomLibraryCallThatUsesNew();
 //  }
-_Ret_notnull_ _Post_writable_byte_size_(cbSize) __allocator inline __bcount(cbSize) void * __cdecl operator new(size_t cbSize);
-_Ret_notnull_ _Post_writable_byte_size_(cbSize) __allocator inline __bcount(cbSize) void * __cdecl operator new[](size_t cbSize);
-inline void __cdecl operator delete(void * pv);
-inline void __cdecl operator delete[](void *pv);
+_Ret_notnull_ _Post_writable_byte_size_(cbSize) __allocator __bcount(cbSize) void * __cdecl operator new(size_t cbSize);
+_Ret_notnull_ _Post_writable_byte_size_(cbSize) __allocator __bcount(cbSize) void * __cdecl operator new[](size_t cbSize);
+void __cdecl operator delete(void * pv);
+void __cdecl operator delete[](void *pv);
 
 //------------------------------------------------------------------------------
 // Alternatives to new and delete ----------------------------------------------
