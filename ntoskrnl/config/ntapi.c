0bf6dcd659e58220bf136056e6318c45a7349480
Mohammad Amin Mollazadeh <madamin@pm.me>
2025-05-10 15:53:00 +0330

    [NTOS:CM] fix: replace DPRINT with DPRINT1 on errors

--- ntoskrnl/config/ntapi.c
+++ ntoskrnl/config/ntapi.c
@@ -1663,7 +1663,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
 
     if (!NT_SUCCESS(Status))
     {
-        DPRINT("NtNotifyChangeMultipleKeys: failed to open MasterKeyHandle.\n");
+        DPRINT1("NtNotifyChangeMultipleKeys: failed to open MasterKeyHandle. (0x%lx)\n", Status);
         return Status;
     }
 
@@ -1719,7 +1719,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
 
         if (!NT_SUCCESS(Status))
         {
-            DPRINT("NtNotifyChangeMultipleKeys: Probing buffers failed.\n");
+            DPRINT1("NtNotifyChangeMultipleKeys: Probing buffers failed. (0x%lx)\n", Status);
             if (KeyObject)
                 ObDereferenceObject(KeyObject);
             if (LocalSubObjects)
@@ -1777,7 +1777,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
                                         &SubHandle);
             if (!NT_SUCCESS(Status))
             {
-                DPRINT("NtNotifyChangeMultipleKeys: Failed to open subordinate object handle.\n");
+                DPRINT1("NtNotifyChangeMultipleKeys: Failed to open subordinate object handle. (0x%lx)\n", Status);
                 goto Failure;
             }
 
@@ -1791,14 +1791,14 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
             ZwClose(SubHandle);
             if (!NT_SUCCESS(Status))
             {
-                DPRINT("NtNotifyChangeMultipleKeys: Failed to reference subordinate object.\n");
+                DPRINT1("NtNotifyChangeMultipleKeys: Failed to reference subordinate object. (0x%lx)\n", Status);
                 goto Failure;
             }
 
             /* Check if this is a duplicate object */
             if (LocalSubObjectsKeyBody[i]->KeyControlBlock == KeyObject->KeyControlBlock)
             {
-                DPRINT("NtNotifyChangeMultipleKeys: Duplicate subordinate object found.\n");
+                DPRINT1("NtNotifyChangeMultipleKeys: Duplicate subordinate object found.\n");
                 Status = STATUS_INVALID_PARAMETER;
                 goto Failure;
             }
@@ -1807,7 +1807,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
             {
                 if (LocalSubObjectsKeyBody[i]->KeyControlBlock == LocalSubObjectsKeyBody[j]->KeyControlBlock)
                 {
-                    DPRINT("NtNotifyChangeMultipleKeys: Duplicate subordinate object found.\n");
+                    DPRINT1("NtNotifyChangeMultipleKeys: Duplicate subordinate object found.\n");
                     Status = STATUS_INVALID_PARAMETER;
                     goto Failure;
                 }
@@ -1828,7 +1828,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
         Status = CmpInsertNotifyBlock(KeyObject, CompletionFilter, WatchTree, &NotifyBlock);
         if (!NT_SUCCESS(Status))
         {
-            DPRINT("NtNotifyChangeMultipleKeys: Failed to allocate and insert NotifyBlock. (0x%lx)\n", Status);
+            DPRINT1("NtNotifyChangeMultipleKeys: Failed to allocate and insert NotifyBlock. (0x%lx)\n", Status);
             goto Failure;
         }
         KeyObject->NotifyBlock = NotifyBlock;
@@ -1848,7 +1848,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
                                                 &LocalEventHandle);
         if (!NT_SUCCESS(Status))
         {
-            DPRINT("NtNotifyChangeMultipleKeys: CmpConvertHandleToKernelHandle failed with 0x%lx.\n", Status);
+            DPRINT1("NtNotifyChangeMultipleKeys: CmpConvertHandleToKernelHandle failed. (0x%lx)\n", Status);
             goto Failure;
         }
 
@@ -1860,7 +1860,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
                                            NULL);
         if (!NT_SUCCESS(Status))
         {
-            DPRINT("NtNotifyChangeMultipleKeys: Failed to reference event object.\n");
+            DPRINT1("NtNotifyChangeMultipleKeys: Failed to reference event object. (0x%lx)\n", Status);
         }
         ZwClose(LocalEventHandle);
         LocalEventHandle = NULL;
@@ -1871,7 +1871,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
         Status = CmpCreateEvent(NotificationEvent, &LocalEventHandle, &LocalEventObject);
         if (!NT_SUCCESS(Status))
         {
-            DPRINT("NtNotifyChangeMultipleKeys: Failed to create event object for synchronous call.\n");
+            DPRINT1("NtNotifyChangeMultipleKeys: Failed to create event object for synchronous call. (0x%lx)\n", Status);
             goto Failure;
         }
         ZwClose(LocalEventHandle);
@@ -1886,7 +1886,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
                                 &PostBlock);
     if (!NT_SUCCESS(Status))
     {
-        DPRINT("NtNotifyChangeMultipleKeys: Failed to allocate and insert master PostBlock. (0x%lx)\n", Status);
+        DPRINT1("NtNotifyChangeMultipleKeys: Failed to allocate and insert master PostBlock. (0x%lx)\n", Status);
         goto Failure;
     }
     
@@ -1919,7 +1919,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
                                           &SubNotifyBlocks[i]);
             if (!NT_SUCCESS(Status))
             {
-                DPRINT("NtNotifyChangeMultipleKeys: Failed to allocate and insert subordinate NotifyBlock. (0x%lx)\n", Status);
+                DPRINT1("NtNotifyChangeMultipleKeys: Failed to allocate and insert subordinate NotifyBlock. (0x%lx)\n", Status);
                 goto Failure;
             }
 
@@ -1931,7 +1931,7 @@ NtNotifyChangeMultipleKeys(_In_ HANDLE MasterKeyHandle,
                                            &SubPostBlock);
             if (!NT_SUCCESS(Status))
             {
-                DPRINT("NtNotifyChangeMultipleKeys: Failed to allocate and insert subordinate PostBlock. (0x%lx)\n", Status);
+                DPRINT1("NtNotifyChangeMultipleKeys: Failed to allocate and insert subordinate PostBlock. (0x%lx)\n", Status);
                 goto Failure;
             }
             
