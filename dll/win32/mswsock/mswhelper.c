7efbe34a4bf12a27ec5b425edebc253a54afbdb7
Mikhail Tyukin <mishakeys20@gmail.com>
2025-06-15 19:25:19 -0400

    [MSWSOCK] Fix integer overflow when running unity games

--- dll/win32/mswsock/mswhelper.c
+++ dll/win32/mswsock/mswhelper.c
@@ -104,7 +104,7 @@ BOOL
 mswBufferAppendLst(_Inout_ PMSW_BUFFER mswBuf,
                    _In_ void **lst,
                    _In_ DWORD itemByteLength,
-                   _In_opt_ int ptrofs)
+                   _In_opt_ LONGLONG ptrofs)
 {
     DWORD lstItemCount;
     DWORD lstByteSize;
@@ -277,7 +277,7 @@ mswBufferAppendBlob_Hostent(_Inout_ PMSW_BUFFER mswBuf,
 
     phe->h_addr_list = (char**)(mswBufferEndPtr(mswBuf) - bytesOfs);
 
-    if (!mswBufferAppendLst(mswBuf, lst, 4, -(LONG_PTR)bytesOfs))
+    if (!mswBufferAppendLst(mswBuf, lst, sizeof(*lst), -(LONG_PTR)bytesOfs))
         return FALSE;
 
     /* name */
