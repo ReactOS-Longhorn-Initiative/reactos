7ede9254131a5d068231e0277a069af553812175
Mikhail Tyukin <mishakeys20@gmail.com>
2025-09-23 08:10:35 -0400

    reenable win10

--- sdk/include/psdk/ntverp.h
+++ sdk/include/psdk/ntverp.h
@@ -18,14 +18,14 @@
 #if (TARGET_VERSION >= _WIN32_WINNT_WIN7)
 
 //
-// Windows NT 6.1.7601.17514
+// Windows NT 10.0.10240.16384
 //
-#define VER_PRODUCTBUILD                    7601
-#define VER_PRODUCTBUILD_QFE                17514
-#define VER_PRODUCTMAJORVERSION             6
-#define VER_PRODUCTMINORVERSION             1
+#define VER_PRODUCTBUILD                    10240
+#define VER_PRODUCTBUILD_QFE                16384
+#define VER_PRODUCTMAJORVERSION             10
+#define VER_PRODUCTMINORVERSION             0
 #define VER_PRODUCTVERSION_W                (0x0601)
-#define VER_PRODUCTVERSION_DW               (0x06010000 | VER_PRODUCTBUILD)
+#define VER_PRODUCTVERSION_DW               (0x0A000000 | VER_PRODUCTBUILD)
 
 #elif (TARGET_VERSION >= _WIN32_WINNT_VISTA)
 
