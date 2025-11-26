5189f0cf6f4a05b99a1f5a0e2d7cad9c56b5f450
Justin Miller <justin.miller@reactos.org>
2025-11-25 20:41:36 -0800

    [SHELL32][SDK] Shell32 patch from oleg cleaned

--- dll/win32/shell32/wine/pidl.c
+++ dll/win32/shell32/wine/pidl.c
@@ -1665,14 +1665,7 @@ HRESULT WINAPI SHGetNameFromIDList(PCIDLIST_ABSOLUTE pidl, SIGDN sigdnName, PWST
     return ret;
 }
 
-/*************************************************************************
- * SHGetItemFromDataObject          [SHELL32.@]
- */
-HRESULT WINAPI SHGetItemFromDataObject(IDataObject *pdtobj, DATAOBJ_GET_ITEM_FLAGS dwFlags,
-                                       REFIID riid, void **ppv)
-{
-    return E_NOTIMPL; // FIXME
-}
+//#ifndef __REACTOS__
 
 /*************************************************************************
  * SHGetIDListFromObject             [SHELL32.@]
@@ -1749,6 +1742,8 @@ HRESULT WINAPI SHGetIDListFromObject(IUnknown *punk, PIDLIST_ABSOLUTE *ppidl)
 
     return ret;
 }
+
+//#endif /* !__REACTOS__ */
 
 /**************************************************************************
  *
