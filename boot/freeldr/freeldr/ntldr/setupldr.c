057e5348d762b498cc64ebfd89bc8f2ea625f192
Mikhail Tyukin <mishakeys20@gmail.com>
2025-11-26 11:26:46 -0500

    [NTOSKRNL][NTDLL][FREELDR] Switch to Win10-1507 syscalls/NT version

--- boot/freeldr/freeldr/ntldr/setupldr.c
+++ boot/freeldr/freeldr/ntldr/setupldr.c
@@ -696,7 +696,7 @@ LoadReactOSSetup(
         UiResetForSOS();
 
     /* Allocate and minimally-initialize the Loader Parameter Block */
-    AllocateAndInitLPB(_WIN32_WINNT_WS03, &LoaderBlock);
+    AllocateAndInitLPB(TARGET_VERSION, &LoaderBlock);
 
     /* Allocate and initialize the setup loader block */
     SetupBlock = &WinLdrSystemBlock->SetupBlock;
@@ -734,7 +734,7 @@ LoadReactOSSetup(
     UiDrawStatusText("The Setup program is starting...");
 
     /* Finish loading */
-    return LoadAndBootWindowsCommon(_WIN32_WINNT_WS03,
+    return LoadAndBootWindowsCommon(TARGET_VERSION,
                                     LoaderBlock,
                                     BootOptions,
                                     SystemPartition,
