9b35291376d4a7dcf13348f0f6ee83befb898d90
Mikhail Tyukin <mishakeys20@gmail.com>
2025-12-24 11:45:31 -0500

    [DWRITE] WIP Sync

--- dll/directx/wine/dwrite/freetype.c
+++ dll/directx/wine/dwrite/freetype.c
@@ -57,6 +57,7 @@ typedef struct
     FT_Int patch;
 } FT_Version_t;
 
+#ifndef __REACTOS__
 #define MAKE_FUNCPTR(f) static typeof(f) * p##f = NULL
 MAKE_FUNCPTR(FT_Activate_Size);
 MAKE_FUNCPTR(FT_Done_Face);
@@ -87,6 +88,7 @@ MAKE_FUNCPTR(FT_Outline_Transform);
 MAKE_FUNCPTR(FT_Outline_Translate);
 MAKE_FUNCPTR(FT_Set_Pixel_Sizes);
 #undef MAKE_FUNCPTR
+#endif
 static FT_Error (*pFT_Outline_EmboldenXY)(FT_Outline *, FT_Pos, FT_Pos);
 
 #define FaceFromObject(o) ((FT_Face)(ULONG_PTR)(o))
@@ -117,6 +119,7 @@ static NTSTATUS process_attach(void *args)
 {
     FT_Version_t FT_Version;
 
+#ifndef __REACTOS__
     ft_handle = dlopen(SONAME_LIBFREETYPE, RTLD_NOW);
     if (!ft_handle)
     {
@@ -155,6 +158,7 @@ static NTSTATUS process_attach(void *args)
     LOAD_FUNCPTR(FT_Set_Pixel_Sizes)
 #undef LOAD_FUNCPTR
     pFT_Outline_EmboldenXY = dlsym(ft_handle, "FT_Outline_EmboldenXY");
+#endif
 
     if (pFT_Init_FreeType(&library) != 0)
     {
@@ -168,11 +172,13 @@ static NTSTATUS process_attach(void *args)
     TRACE("FreeType version is %d.%d.%d\n", FT_Version.major, FT_Version.minor, FT_Version.patch);
     return STATUS_SUCCESS;
 
+#ifndef __REACTOS__
 sym_not_found:
     WINE_MESSAGE("Wine cannot find certain functions that it needs from FreeType library.\n");
     dlclose(ft_handle);
     ft_handle = NULL;
     return STATUS_UNSUCCESSFUL;
+#endif
 }
 
 static NTSTATUS process_detach(void *args)
@@ -815,7 +821,11 @@ const unixlib_entry_t __wine_unix_call_funcs[] =
     get_design_glyph_metrics,
 };
 
+#ifdef __REACTOS__
+C_ASSERT( sizeof(__wine_unix_call_funcs) / sizeof(__wine_unix_call_funcs[0]) == unix_funcs_count );
+#else
 C_ASSERT( ARRAYSIZE(__wine_unix_call_funcs) == unix_funcs_count );
+#endif
 
 #ifdef _WIN64
 
@@ -1043,6 +1053,10 @@ const unixlib_entry_t __wine_unix_call_wow64_funcs[] =
     wow64_get_design_glyph_metrics,
 };
 
+#ifdef __REACTOS__
+C_ASSERT( sizeof(__wine_unix_call_wow64_funcs) / sizeof(__wine_unix_call_wow64_funcs[0]) == unix_funcs_count );
+#else
 C_ASSERT( ARRAYSIZE(__wine_unix_call_wow64_funcs) == unix_funcs_count );
+#endif
 
 #endif  /* _WIN64 */
