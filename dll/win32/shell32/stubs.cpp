6285a808b75d2313b9ef0ede8e651ddd2af6a29c
Mikhail Tyukin <mishakeys20@gmail.com>
2026-01-25 20:50:26 -0500

    [SHELL32] fix GetCurrentProcessExplicitAppUserModelID crash

--- dll/win32/shell32/stubs.cpp
+++ dll/win32/shell32/stubs.cpp
@@ -810,7 +810,8 @@ WINAPI
 GetCurrentProcessExplicitAppUserModelID(PWSTR *AppID)
 {
     FIXME("GetCurrentProcessExplicitAppUserModelID() stub\n");
-    return S_OK;
+    *AppID = NULL;
+    return E_NOTIMPL;
 }
 
 /*
