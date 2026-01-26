8a2bd49dcd8d2401cfd8ed43e0325231bd215317
Mikhail Tyukin <mishakeys20@gmail.com>
2026-01-25 20:50:45 -0500

    [NTDLL] fix uninitialized critical section

--- dll/ntdll/ldr/ldrinit.c
+++ dll/ntdll/ldr/ldrinit.c
@@ -76,6 +76,7 @@ RTL_CRITICAL_SECTION LdrpLoaderLock =
     0
 };
 RTL_CRITICAL_SECTION FastPebLock;
+extern RTL_CRITICAL_SECTION LdrpDllDirectoryLock;
 
 BOOLEAN ShowSnaps;
 
@@ -2139,6 +2140,7 @@ LdrpInitializeProcess(IN PCONTEXT Context,
     /* Setup Fast PEB Lock */
     RtlInitializeCriticalSection(&FastPebLock);
     Peb->FastPebLock = &FastPebLock;
+    RtlInitializeCriticalSection(&LdrpDllDirectoryLock);
     //Peb->FastPebLockRoutine = (PPEBLOCKROUTINE)RtlEnterCriticalSection;
     //Peb->FastPebUnlockRoutine = (PPEBLOCKROUTINE)RtlLeaveCriticalSection;
 
