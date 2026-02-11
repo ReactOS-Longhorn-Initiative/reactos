4b876f674940316a54406431348a402fec78ffc4
Adam Słaboń <asaillen@protonmail.com>
2026-02-11 23:50:42 +0100

    [NTOS:MM][WIP] Dynamically allocate the paged pool on 64 bit

--- ntoskrnl/mm/ARM3/mminit.c
+++ ntoskrnl/mm/ARM3/mminit.c
@@ -1806,6 +1806,13 @@ MiBuildPagedPool(VOID)
     //
     Size = MmSizeOfPagedPoolInBytes;
 
+#ifdef _WIN64
+    //
+    // Simple calculation of paged pool size based on available pages.
+    //
+    Size *= ((MmNumberOfPhysicalPages * PAGE_SIZE) / _1GB);
+#endif
+
     if (Size < MI_MIN_INIT_PAGED_POOLSIZE) Size = MI_MIN_INIT_PAGED_POOLSIZE;
     NumberOfPages = BYTES_TO_PAGES(Size);
 
