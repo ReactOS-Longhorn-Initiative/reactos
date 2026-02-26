d3ed0123e0881af4925c0f7dffee3d81319763bf
Mikhail Tyukin <mishakeys20@gmail.com>
2026-02-26 16:10:24 -0500

    [NTUSER] fix bug

--- win32ss/user/ntuser/window.c
+++ win32ss/user/ntuser/window.c
@@ -4252,7 +4252,8 @@ NtUserQueryWindow(HWND hWnd, DWORD Index)
          }
          else
          {
-            Result = (DWORD_PTR)IntGetWndThreadId(pWnd);
+            if (pWnd->head.pti != NULL)
+               Result = (DWORD_PTR)IntGetWndThreadId(pWnd);
          }
          break;
       }
