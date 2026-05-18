eac41a7c205fbbdf85177d58f984c00524a21229
Adam Słaboń <asaillen@protonmail.com>
2026-05-18 21:42:12 +0200

    Revert "Revert to NT 5.2 for Nvidia driver"
    This reverts commit dde1cb2eb86e05aa3059713bcfc75299ce125694.

--- sdk/include/psdk/ntverp.h
+++ sdk/include/psdk/ntverp.h
@@ -13,8 +13,7 @@
  * PROGRAMMERS:     Alex Ionescu (alex.ionescu@reactos.org)
  */
 
-#if 0
-#define  TARGET_VERSION _WIN32_WINNT_WS03
+#define  TARGET_VERSION _WIN32_WINNT_WIN10
 
 #if (TARGET_VERSION >= _WIN32_WINNT_WIN10)
 
@@ -39,21 +38,6 @@
 #define VER_PRODUCTMINORVERSION             0
 #define VER_PRODUCTVERSION_W                (0x0600)
 #define VER_PRODUCTVERSION_DW               (0x06000000 | VER_PRODUCTBUILD)
-#else
-//
-// Windows NT Build 3790.3959
-//
-#define VER_PRODUCTBUILD                    3790
-#define VER_PRODUCTBUILD_QFE                3959
-
-//
-// Windows NT Version 5.2
-//
-#define VER_PRODUCTMAJORVERSION             5
-#define VER_PRODUCTMINORVERSION             2
-#define VER_PRODUCTVERSION_W                (0x0502)
-#define VER_PRODUCTVERSION_DW               (0x05020000 | VER_PRODUCTBUILD)
-#endif
 #else
 //
 // Windows NT Build 3790.3959
