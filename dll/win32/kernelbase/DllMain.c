153027edb69e27feeb6dc12e7b9d053a1ec43d1e
Justin Miller <justin.miller@reactos.org>
2026-07-27 15:54:51 -0700

    kernelbase changes

--- dll/win32/kernelbase/DllMain.c
+++ dll/win32/kernelbase/DllMain.c
@@ -6,11 +6,24 @@ void init_locale( HMODULE module );
 // Used by wine/locale.c
 char system_dir[MAX_PATH];
 
+/* Wine defines this in kernelbase's main.c, which we cannot build yet (it
+ * needs init_locale(), still inside locale.c's #ifndef __REACTOS__ block).
+ * file.c and process.c read it, so provide it here until main.c comes up. */
+BOOL is_wow64 = FALSE;
+
 static
 void
 InitSystemDir(void)
 {
     GetSystemDirectoryA(system_dir, MAX_PATH);
+}
+
+static
+void
+InitIsWow64(void)
+{
+    if (!IsWow64Process(GetCurrentProcess(), &is_wow64))
+        is_wow64 = FALSE;
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
@@ -19,6 +32,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     {
         case DLL_PROCESS_ATTACH:
             InitSystemDir();
+            InitIsWow64();
             //init_locale(hinstDLL);
             break;
 
