7e34a937ea883117413768ee960eb0112a6e2d5f
Mikhail Tyukin <mishakeys20@gmail.com>
2026-08-17 21:39:43 -0400

    [ALPC] fix build

--- ntoskrnl/alpc/alpc.h
+++ ntoskrnl/alpc/alpc.h
@@ -441,8 +441,8 @@ AlpcpClosePort(
     _In_opt_ PEPROCESS Process,
     _In_ PVOID Object,
     _In_ ACCESS_MASK GrantedAccess,
-    _In_ ULONG ProcessHandleCount,
-    _In_ ULONG SystemHandleCount);
+    _In_ ULONG_PTR ProcessHandleCount,
+    _In_ ULONG_PTR SystemHandleCount);
 
 NTSTATUS
 NTAPI
